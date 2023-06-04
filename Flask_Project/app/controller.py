6852d4a33c5beb86fee27da413e5c0f5b269a60f
Nubiss <120053903+Nubiss@users.noreply.github.com>
2023-06-04 19:31:36 +0300

    Add files via upload
    Python_ЛР_3

---
+++ Flask_Project/app/controller.py
@@ -0,0 +1,20 @@
+from app import app
+from flask import render_template, request
+from datetime import datetime
+from.forms import UserNameForm
+
+@app.route ('/', methods=['GET'])
+@app.route('/index')
+def index():
+        form = UserNameForm()
+        return render_template ("index.html", form = form);
+
+@app.route ('/', methods=['POST'])
+def index_post():
+#         text = request. form ['user_name']
+        form = UserNameForm ()
+        if form.validate_on_submit ():
+        text = form.UserNameField.data
+        return render_template ("index.html", form = form, user_name = text);
+else:
+        return "Bad form"
